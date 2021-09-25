55bfe1ee0896e7e260bd3cd0543e609b477c7bf6
jp val <valdespino.jp@knights.ucf.edu>
2021-09-24 23:29:20 -0400

    permutations

---
+++ permutations.cpp
@@ -0,0 +1,29 @@
+// JP Valdespino
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	int n;
+	cin >> n;
+
+	if (n == 1)
+	{
+		cout << "1\n";
+	}
+	else if (n < 4)
+	{
+		cout << "NO SOLUTION\n";
+	}
+	else
+	{
+		for (int i = 2; i <= n; i+=2)
+			cout << i << " ";
+		
+		for (int i = 1; i <= n; i+=2)
+			cout << i << ((i+2 <= n) ? " " : "\n");
+	}
+	
+	return 0;
+}
